0820217f30b3b0bbbd716a71fc7e185b98c6963c
J-Noel1523 <noel.jerry94@gmail.com>
2018-12-06 08:12:25 -0500

    ss

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ var Messages = mongoose.model('messages', {
      var path = files.myfile.path;
      cloudinary.v2.uploader.upload(path, {folder: "chatpictures"},function(error, result) {
        console.log(result.url, error);
-       var images = new Images({name: util.inspect(req.body.name.toString()), url:result.url});
+       var images = new Images({name: util.inspect(req.body.name), url:result.url});
        images.save().then(function(){
          console.log('picture/vid sent');
        });
