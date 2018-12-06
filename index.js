5e0b0c3ff4561af339723eb32d84d6607c1b11d3
J-Noel1523 <noel.jerry94@gmail.com>
2018-12-05 19:35:17 -0500

    tt

--- index.js
+++ index.js
@@ -100,9 +100,9 @@ app.get("/chats", (req, res) => {
 app.get('/images', function(req, res){
 
   cloudinary.v2.api.resources({ type: 'upload' }, function(error, result){
-
+ console.log(result);
   });
-  res.send(cloudinary.v2.api.resources({ type: 'upload' }, function(error, result){ console.log(result);}));
+  res.send(cloudinary.v2.api.resources({ type: 'upload' }, function(error, result){}));
 });
 
 io.on("connection", function(socket, error){
