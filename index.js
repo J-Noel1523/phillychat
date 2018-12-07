53c60e0a4ed1a2f58a6eee0a830a5f5060a70e66
J-Noel1523 <noel.jerry94@gmail.com>
2018-12-06 21:42:06 -0500

    ss

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ var Messages = mongoose.model('messages', {
        cloudinary.v2.uploader.upload(path, {folder: "chatpictures"},function(error, result) {
          console.log(result.url, error);
          longUrl = result.url;
-         longUrl = '<img src="' + longUrl + '"/>';
+         longUrl = '<img style="height:250px;width:300px;"src="' + longUrl + '"/>';
         var images = new Images({name: 'Image', url:result.url});
          console.log(images);
          images.save().then(function(){
